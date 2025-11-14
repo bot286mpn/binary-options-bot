aedaa240b456b5d7ef40cd17380f54dfe72f9408
bot286mpn <bot286@gmail.com>
2025-11-14 16:37:09 +0300

    Перемещён utils/screen_capture.py из trading_bot/utils/ в корень (utils/).

---
+++ utils/screen_capture.py
@@ -0,0 +1,8 @@
+# screen_capture.py базовый шаблон
+
+class ScreenCapture:
+    def __init__(self):
+        pass
+    def capture(self, region):
+        # TODO: Capture selected screen region
+        pass
